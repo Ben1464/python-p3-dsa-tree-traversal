4875b64e067e4635145cee726b6bf545f4794c71
Ben <kimaniben14@gmail.com>
2024-07-30 07:34:42 +0100

    Completed assignment

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
-
-  def get_element_by_id(self, id):
-    pass
+    def __init__(self, root):
+        self.root = root
+    
+    def get_element_by_id(self, element_id):
+        return self._get_element_by_id(self.root, element_id)
+    
+    def _get_element_by_id(self, node, element_id):
+        if node['id'] == element_id:
+            return node
+        
+        for child in node.get('children', []):
+            result = self._get_element_by_id(child, element_id)
+            if result:
+                return result
+        
+        return None
